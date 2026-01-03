7c7f26ea6878690e0fd546738877d7477e38d5d2
s1n1s_01 <alexandrinaovchinnikova@gmail.com>
2026-01-03 21:36:33 +0200

    start mockUp + add img

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.write("Hello, World!");
+const inputImg = document.getElementById("inputImg");
+const previewImg = document.getElementById("previewImg");
+
+inputImg.addEventListener("change", (e) => {
+    const file = inputImg.files[0];
+
+    if (!file) return;
+
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.style.display = "block";
+})
